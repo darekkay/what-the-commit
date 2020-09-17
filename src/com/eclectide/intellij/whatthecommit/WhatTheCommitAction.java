21c7429f33a7414dfd81051a913a95341d880b63
Darek Kay <darekkay@gmail.com>
2020-09-17 22:04:02 +0200

    Update outdated author information

--- src/com/eclectide/intellij/whatthecommit/WhatTheCommitAction.java
+++ src/com/eclectide/intellij/whatthecommit/WhatTheCommitAction.java
@@ -1,26 +1,3 @@
-/*
- * Copyright 2014 Darek Kay <darekkay@eclectide.com>
- *
- * MIT license
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies
- * or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package com.eclectide.intellij.whatthecommit;
 
 import com.intellij.openapi.actionSystem.AnAction;
@@ -43,9 +20,6 @@ import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-/**
- * @author darekkay
- */
 public class WhatTheCommitAction extends AnAction implements DumbAware {
 
     private static final String URL = "http://whatthecommit.com/index.txt";
@@ -83,7 +57,7 @@ public class WhatTheCommitAction extends AnAction implements DumbAware {
 
     public String loadCommitMessage(final String url) {
         final FutureTask<String> downloadTask = new FutureTask<String>(new Callable<String>() {
-            public String call() {		
+            public String call() {
 				try {
 				  final HttpConfigurable httpConfigurable = HttpConfigurable.getInstance();
 				  httpConfigurable.prepareURL(url);
